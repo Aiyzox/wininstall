2fc8e3c2639d8ddc51083986ac7c8da4286d5a09
Aiyzox <charlie.gourdien@gmail.com>
2022-08-30 20:02:06 +0200

    Ajout d'une bannière au menu de choix

--- wininstall.bat
+++ wininstall.bat
@@ -2,6 +2,12 @@
 
 :start
 cls
+echo "__        __ _          ___              _          _  _ "
+echo "\ \      /  (_)  _ __  |_ _| _ __   ___ | |_  __ _ | || |"
+echo " \ \ /\ / / | | | '_ \  | | | '_ \ / __|| __|/ _` || || |"
+echo "  \ V  V /  | | | | | | | | | | | \ __ \\ ||  (_| || || |"
+echo "   \_/\_/   |_| |_| |_||___||_| |_||___/ \__|\__,_||_||_|"
+echo.
 echo Bienvenue sur WinInstall !
 echo.
 echo 1. Installation automatique
